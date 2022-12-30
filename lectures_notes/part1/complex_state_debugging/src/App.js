273671e4df66004cdf4c2fd79fc1f179f5d2b547
sumalemambo <ignacio.quintana@usm.cl>
2022-12-30 20:38:55 -0300

    Event Handling Revisited

--- lectures_notes/part1/complex_state_debugging/src/App.js
+++ lectures_notes/part1/complex_state_debugging/src/App.js
@@ -342,6 +342,46 @@ const App = () => {
   )
 }
 
+# Debugging React Applications
+
+# Rules of Hooks
+
+The are a few limitations and rules we have to follow to
+ensure that our application uses hooks-based state functions
+correctly.
+
+The useState function (as well as the useEffect function
+introduced later on in the course) must NOT be called from
+inside of a loop, a conditional expression, or any place
+that is not a function defining a component.
+
+To recap, hooks may only be called from the inside of a
+function body that defines a React component:
+
+const App = () => {
+  // these are ok
+  const [age, setAge] = useState(0)
+  const [name, setName] = useState('Juha Tauriainen')
+
+  if ( age > 10 ) {
+    // this does not work!
+    const [foobar, setFoobar] = useState(null)
+  }
+
+  for ( let i = 0; i < age; i++ ) {
+    // also this is not good
+    const [rightWay, setRightWay] = useState(false)
+  }
+
+  const notGood = () => {
+    // and this is also illegal
+    const [x, setX] = useState(-1000)
+  }
+
+  return (
+    //...
+  )
+}
 
 
 */
