57c91f846ad56eedf3bca3659d3eb32ac0296ea1
sumalemambo <ignacio.quintana@usm.cl>
2023-01-01 19:30:59 -0300

    A function that returns a function

--- lectures_notes/part1/complex_state_debugging/src/App.js
+++ lectures_notes/part1/complex_state_debugging/src/App.js
@@ -383,6 +383,128 @@ const App = () => {
   )
 }
 
+# Event handling revisited
+
+Let's assume that we're developing this simple application with
+the following component App:
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  return (
+    <div>
+      {value}
+      <button>reset to zero</button>
+    </div>
+  )
+}
+
+We want the clicking of the button to reset the state stored in
+the value variable.
+
+In order to make the button react to a click event, we have to
+add an event handler to it.
+
+Event handlers must always be a function or a reference to a
+function. The button will not work if the event handler is
+set to a variable of any other type.
+
+If we were to define the event handler as a string:
+
+<button onClick="crap...">button</button>
+
+React would warn us about this in the console:
+
+index.js:2178 Warning: Expected `onClick` listener to be a function, instead got a value of `string` type.
+    in button (at index.js:20)
+    in div (at index.js:18)
+    in App (at index.js:27)
+
+The following attemp would also not work:
+
+<button onClick={value + 1}>button</button>
+
+We have attempted to set the event handler to value + 1 which simply
+returns the result of the operation. React will kindly warn us about
+this in the console:
+
+index.js:2178 Warning: Expected `onClick` listener to be a function, instead got a value of `number` type.
+
+This attempt would not work either:
+
+<button onClick={value = 0}>button</button>
+
+The event handler is not a function but a variable assignment, and
+React will once again issue a warning to the console. This attempt
+is also flawed in the sense that we must never mutate state directly
+in React.
+
+What about the following:
+
+<button onClick={console.log('clicked the button')}>
+  button
+</button>
+
+The message gets printed to the console once when the component
+is rendered but nothing happens when we click the button. Why does
+this not work even when our event handler contains a function 
+console.log?
+
+The issue here is that our event handler is defined as a function
+call which means that the event handler is assigned the returned
+value from the function, which in the case of console.log is
+undefined.
+
+The following attempt is flawed as well:
+
+<button onClick={setValue(0)}>button</button>
+
+We have once again tried to set a function call as the event handler.
+
+Executing a particular function call when the button is clicked can
+be accomplished like this:
+
+<button onClick={() => console.log('clicked the button')}>
+  button
+</button>
+
+Now the event handler is a function defined with the arrow
+function syntax () => {console.log('clicked the button')}
+When the component gets rendered, no function gets called
+and only the reference to the arrow function is set to the
+event handler.
+
+We can implement resetting the state in our application
+with this same technique:
+
+<button onClick={() => setValue(0)}>
+  reset
+</button>
+
+Defining the handlers directly in the attribute is not
+necessarily the best possible idea.
+
+You will often see event handlers defined in a separate
+place:
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const handleClick = () => {
+    console.log('clicked the button')
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={handleClick}>button</button>
+    </div>
+  )
+}
+
+# A function that returns a function
+
+
 
 */
 
