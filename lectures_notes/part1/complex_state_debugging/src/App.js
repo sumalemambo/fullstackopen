87fb3f012964306a7e1349c136a6f7cf8f5541f7
sumalemambo <ignacio.quintana@usm.cl>
2023-01-01 20:21:33 -0300

    Passing event handlers to child components

--- lectures_notes/part1/complex_state_debugging/src/App.js
+++ lectures_notes/part1/complex_state_debugging/src/App.js
@@ -504,7 +504,137 @@ const App = () => {
 
 # A function that returns a function
 
+Another way to define an event handler is to use a function that
+returns a function.
 
+Lets make the following changes to our code:
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const hello = () => {
+    const handler = () => console.log('hello world')
+    return handler
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={hello()}>button</button>
+    </div>
+  )
+}
+
+Whats the point of this concept?
+
+Let's change the code a tiny bit:
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
+    </div>
+  )
+}
+
+Functions returning functions can be utilized in defining generic
+functionality that can be customized with parameters. 
+
+Our current definition is slightly verbose:
+
+const hello = (who) => {
+  const handler = () => {
+    console.log('hello', who)
+  }
+  return handler
+}
+
+Let's eliminate helper variables and directly return the created function
+
+const hello = (who) => {
+  return () => {
+    console.log('hello', who)
+  }
+}
+
+Since our hello function is composed of a single return command, we can
+omit the curly braces and use the more compact syntax for arrow functions:
+
+const hello = (who) => 
+    () => {
+    console.log('hello', who)
+  }
+
+Lastly, let's write all of the arrows on the same line:
+
+const hello = (who) => () => {
+    console.log('hello', who)
+}
+
+We can use the same trick to define event handlers that set the state of
+the component to a given value. Let's make the following changes to our
+code:
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const hello = (who) => () => {
+      console.log('hello', who)
+  }
+  
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
+    </div>
+  )
+}
+
+Using functions that returns functions is not required to achieve
+this functionality. Let's return the setValue function which is
+responsible for updating state into a normal function:
+
+const App = () => {
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
+    </div>
+  )
+}
+
+Choosing between the two presented ways of defining your event handlers
+is mostly a matter of taste.
+
+# Passing event handlers to child components
 
 */
 
@@ -536,27 +666,19 @@ const Button = ({ handleClick, text }) => {
 }
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right,setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left'/>
-      <Button handleClick={handleRightClick} text='right'/>
-      {right}
-      <History allClicks={allClicks}/>
+      {value}
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
   )
 }
